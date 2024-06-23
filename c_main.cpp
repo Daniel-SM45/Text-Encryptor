c71e267280264f3297a33169e32d08919b0de6c8
Daniel-SM45 <134079142+Daniel-SM45@users.noreply.github.com>
2024-06-23 12:50:12 -0300

    Update c_main.cpp
    Minor changes to main.cpp

--- c_main.cpp
+++ c_main.cpp
@@ -1,16 +1,15 @@
 #include<iostream>
 #include<stdlib.h>
 #include"c_source_data.h"
-using namespace std;
 
 int main() {
 	main_title();
 	char user_op_input, user_op_end_input;
-	cout << " >>>: ";
-	cin >> user_op_input;
+	std::cout << " >>>: ";
+	std::cin >> user_op_input;
 	user_option(user_op_input);
-	cout << " Continuar com o programa ? [S] [N]\n";
-	cin >> user_op_end_input;
-	user_program_end_dialoge(user_op_end_input);
+	std::cout << " Continuar com o programa ? [S] [N]\n";
+	std::cin >> user_op_end_input;
+	user_program_end_dialog(user_op_end_input);
 	return 0;
 }
